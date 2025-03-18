cdcdb8e05f882bfeee6c13db2c7ccd64f2af7eaa
Jorge <jorgeflorespartida1234@gmail.com>
2025-03-18 17:53:31 -0600

    js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const openModal = document.querySelector('.profile__edit-button');
 const popup     = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close-button');
-
+const guardarPopup = document.querySelector('.popup__form');
 
 openModal.addEventListener('click', (e) => {
     e.preventDefault();
@@ -9,6 +9,11 @@ openModal.addEventListener('click', (e) => {
 });
 
 closePopup.addEventListener('click', (e) => {
+    e.preventDefault();
+    popup.classList.remove('popup--show');
+});
+
+guardarPopup.addEventListener('submit', (e) => {
     e.preventDefault();
     popup.classList.remove('popup--show');
 });
